fd9a9e97f1b70b27af84bf4eb6c366777c047ad9
hoangphuc <hoangphucnb97@gmail.com>
2022-07-14 17:26:01 +0700

    Add librayry UQ112x112

---
+++ contracts/core/libraries/UQ112x112.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+// a library for handling binary fixed point numbers (https://en.wikipedia.org/wiki/Q_(number_format))
+
+// range: [0, 2**112 - 1]
+// resolution: 1 / 2**112
+
+library UQ112x112 {
+    uint224 constant Q112 = 2 ** 112;
+    //encode a uint112 as UQ112x112
+    function encode(uint112 y) internal pure returns (uint224 z){
+        z = uint224(y) * Q112;    //never overflows
+    }
+
+    //devide a UQ112x112 by a unit112, returning a UQ112x112
+    function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
+        z = x / uint224(y);
+    }
+}

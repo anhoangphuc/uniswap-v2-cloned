a23044927db238aa8fe744cd06759a69188d31c6
hoangphuc <hoangphucnb97@gmail.com>
2022-07-18 17:30:07 +0700

    Add IWETH and IUniswapV2Migrator interface

---
+++ contracts/v2-periphery/interfaces/IUniswapV2Migrator.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+interface IUniswapV2Migrator {
+    function migrate(address token, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external;
+}

75b67c13c2a3a7931d83375ca40e66973e9e4aad
hoangphuc <hoangphucnb97@gmail.com>
2022-10-14 09:56:37 +0700

    Add test to check pairAddress created success

--- contracts/core/UniswapV2Factory.sol
+++ contracts/core/UniswapV2Factory.sol
@@ -3,6 +3,7 @@ pragma solidity = 0.8.12;
 
 import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
+import "hardhat/console.sol";
 
 contract UniswapV2Factory is IUniswapV2Factory {
     address private _feeTo;
@@ -36,7 +37,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
         require(_pairs[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS');
-        bytes memory bytecode = type(UniswapV2ERC20).creationCode;
+        bytes memory bytecode = type(UniswapV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
--- test/PairFactory.ts
+++ test/PairFactory.ts
@@ -2,6 +2,7 @@ import { TestERC20, UniswapV2Factory } from "../typechain-types"
 import { SignerWithAddress }from '@nomiclabs/hardhat-ethers/signers';
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import UniswapV2PairBuilt from "../artifacts/contracts/core/UniswapV2Pair.sol/UniswapV2Pair.json";
 
 context("Pair factory", async () => {
     let pairFactory: UniswapV2Factory;
@@ -19,11 +20,36 @@ context("Pair factory", async () => {
         await tokenA.deployed();
         tokenB = await TestERC20.deploy("TokenB", "TKB");
         await tokenB.deployed();
+
+        //Swap two address
+        if (tokenA.address > tokenB.address) [tokenA, tokenB] = [tokenB, tokenA];
     })
 
     it(`Deploy success`, async () => {
         expect(tokenA.address).to.be.properAddress;
         expect(tokenB.address).to.be.properAddress;
         expect(pairFactory.address).to.be.properAddress;
+    })
+
+    it(`Create pair success`, async () => {
+        await pairFactory.createPair(tokenA.address, tokenB.address);
+    })
+
+    it(`Create pair success`, async () => {
+        const deployedByteCode = UniswapV2PairBuilt.bytecode;
+        const salt = ethers.utils.solidityKeccak256(
+            ["address", "address"],
+            [tokenA.address, tokenB.address],
+        );
+
+        const pairAddress = ethers.utils.getCreate2Address(
+            pairFactory.address,
+            salt,
+            ethers.utils.keccak256(deployedByteCode),
+        );
+
+        await expect(pairFactory.createPair(tokenA.address, tokenB.address))
+            .to.be.emit(pairFactory, "PairCreated")
+            .withArgs(tokenA.address, tokenB.address, pairAddress, 1);
     })
 })
